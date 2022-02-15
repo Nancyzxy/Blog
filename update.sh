77b46b2581fa250602e6ea5264b3971ad38564dc
Nancyzxy <11912039@mail.sustech.edu.cn>
2022-02-15 22:38:23 +0800

    rebuilding site Tue Feb 15 22:38:23     2022

---
+++ update.sh
@@ -0,0 +1,31 @@
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+
+
+# build the project
+
+hugo
+
+git add .
+
+
+
+msg="rebuilding site `date`"
+
+
+
+if [ $# -eq 1 ]
+
+  then msg="$1"
+
+fi
+
+
+
+git commit -m "$msg"
+
+
+
+# push source to github
+
+git push origin main:main
